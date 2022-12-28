1b7c28706a2418ad55e61c4fc4ed184d50f655e2
minjiKim87 <68892132+minjiKim87@users.noreply.github.com>
2022-12-28 18:52:47 +0900

    1228

--- Udemy_javascript/app.js
+++ Udemy_javascript/app.js
@@ -1,1 +1,21 @@
-console.log(window.document);
+// console.log(window.document);
+
+// let anchorElement = document.getElementById("external-link");
+// anchorElement.href = "https://google.com";
+
+// anchorElement = document.querySelector("#external-id");
+// anchorElement.href = "https://academind.com";
+
+let h1Element = document.body.firstElementChild;
+h1Element = document.body.firstElementChild[0];
+
+console.dir(h1Element.parentElement);
+console.dir(h1Element.nextElementSibling);
+
+h1Element = document.body.firstElementChild[0];
+
+let highlightedParagraph = document.querySelector(".highlighted-paragraph");
+
+let newAnchorElement = document.createElement("a");
+let firstParagraph = document.querySelector("p");
+firstParagraph.append(newAnchorElement);
