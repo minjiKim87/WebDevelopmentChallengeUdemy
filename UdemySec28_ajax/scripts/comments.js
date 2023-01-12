12b23f32ab9c36292e9cb8e1a8f3e902bb7d5d94
minjiKim87 <68892132+minjiKim87@users.noreply.github.com>
2023-01-12 15:53:41 +0900

    0112

---
+++ UdemySec28_ajax/scripts/comments.js
@@ -0,0 +1,76 @@
+const loadCommentsBtnElement = document.getElementById("load-comments-btn");
+const commentSectionElement = document.getElementById("comments");
+const commentsFormElement = document.querySelector("#comments-form form");
+const commentTitleElement = document.getElementById("title");
+const commentTextElement = document.getElementById("text");
+function createCommentsList() {
+  const commentListElement = document.createElement("ol");
+
+  for (const comments of comments) {
+    const commentElement = document.createElement("li");
+    commentElement.innerHtml = `
+    <article class="comment-item">
+    <h2>${comment.title}</h2>
+    <p>${comment.text}</p>
+  </article>
+  `;
+    commentListElement.appendChild(commentElement);
+  }
+
+  return commentListElement;
+}
+
+async function fetchCommentsForPost() {
+  const postId = loadCommentsBtnElement.dataset.postid;
+  const response = await fetch(`/posts/${postId}/comments`);
+  try{
+    
+  if(!response.ok){
+    alert('Fetching comments failed!');
+    return;
+  }
+  const responseData = await response.json();
+
+  if (responseData && responseData.length > 0) {
+    const commentListElement = createCommentsList(responseData);
+    commentSectionElement.innerHTML = "";
+    commentSectionElement.appendChild(commentsListElement);
+  } else {
+    commentsSectionElement.firstElementChild.textContent =
+      "We could not find any comments. Maybe add one?";
+  }
+  }catch(error){
+    alert('Getting comments failed!');
+  }
+  
+}
+
+async function saveComment(event) {
+  event.preventDefault();
+  const postId = loadCommentsBtnElement.dataset.postid;
+
+  const enteredTitle = commentTitleElement.value;
+  const enteredText = commentTextElement.value;
+
+  const comment = { title: enteredTitle, text: enteredText };
+
+  const response = await fetch(`/posts/${postId}/comments`, {
+    method: "POST",
+    body: JSON.stringify(comment),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}catch(error){
+  if (response.ok) {
+    fetchCommentsForPost();
+  } else {
+    alert("Could not send comment!");
+  }
+}
+
+  
+
+
+loadCommentsBtnElement.addEventListener("click", fetchCommentsForPost);
+commentsFormElement.addEventListener("submit", saveComment);
